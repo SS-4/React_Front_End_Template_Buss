0d787ccbbc9b2d88ac523cfb9e74c4edeea28f01
SSTSSSS <kian.tafemail@gmail.com>
2022-09-17 18:57:44 +1000

    changed video

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -84,7 +84,7 @@ const Hero = ({
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
 
           <video autoPlay muted >
-            <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"/>
+            <source src="https://www.youtube.com/watch?v=9LqUiXoT2XQ&ab_channel=TRADEONE"/>
           </video>
           </div>
           <Modal
