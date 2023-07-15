563700204a53255c077c027aa519a9c3a42a4f9b
SS <60110107+SS-4@users.noreply.github.com>
2023-07-15 18:17:49 +1000

    cl2

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -62,7 +62,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Welcome To Gaming World <span className="text-color-primary">People!</span>
+              Welcome To Gaming <span className="text-color-primary">People!</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
